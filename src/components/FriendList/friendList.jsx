eb0b08dc6a67b60188ddb161f1d82565e5550851
Michał Błaszczyk <michal.blaszczyk00@gmail.com>
2024-01-31 21:00:11 +0100

    homework first attempt

---
+++ src/components/FriendList/friendList.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import css from './FriendList.module.css';
+import PropTypes from 'prop-types';
+
+const FriendList = ({ friends }) => {
+  return (
+    <div className={css.wrapper}>
+      <ul className={css.friendList}>
+        {friends.map(friend => (
+          <FriendListEl
+            avatar={friend.avatar}
+            name={friend.name}
+            isOnline={friend.isOnline}
+            key={friend.id}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+FriendList.propTypes = {
+  friends: PropTypes.array,
+};
+
+const FriendListEl = ({ avatar, name, isOnline }) => {
+  return (
+    <li className={css.item}>
+      <span
+        className={`${css.status} ${isOnline ? css.online : css.offline}`}
+      ></span>
+      <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
+      <p className={css.name}>{name}</p>
+    </li>
+  );
+};
+FriendListEl.propTypes = {
+  avatar: PropTypes.string,
+  name: PropTypes.string,
+  isOnline: PropTypes.bool,
+};
+
+export default FriendList;
---
+++ src/components/Statistics/statistics.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import css from './Statistics.module.css';
+import PropTypes from 'prop-types';
+
+const Statistics = ({ title, stats }) => {
+  return (
+    <div className={css.wrapper}>
+      <section className={css.statistics}>
+        {title && <h2 className={css.title}>{title}</h2>}
+
+        <ul className={css.statList}>
+          {stats.map(stat => (
+            <li className={css.item} key={stat.id}>
+              <span className={css.label}>{stat.label}</span>
+              <span className={css.percentage}>{stat.percentage}</span>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </div>
+  );
+};
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.array,
+};
+
+export default Statistics;
