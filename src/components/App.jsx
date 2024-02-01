a6ae6c614f0cb9d5bcbe6f1df326fdbe3e056aaa
Michał Błaszczyk <michal.blaszczyk00@gmail.com>
2024-02-01 20:23:33 +0100

    homework attempt v2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,18 +4,21 @@ import Statistics from './Statistics/statistics';
 import data from './Statistics/data.json';
 import FriendList from './FriendList/friendList'
 import friends from './FriendList/friends.json'
+import TransactionsInfo from './Transaction/transactionsData'
+import transactions from './Transaction/transactions.json'
+
 
 export const App = () => {
   return (
     <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
+    style={{
+      height: '100vh',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      fontSize: 40,
+      color: '#010101',
+    }}
     >
       <Profile
         username={user.username}
@@ -25,7 +28,8 @@ export const App = () => {
         stats={user.stats}
       />
       <Statistics title="Upload stats" stats={data} />
-      <FriendList friends={friends} />;
+      <FriendList friends={friends} />
+      <TransactionsInfo items={transactions} />
     </div>
   );
 };
---
+++ src/components/Transaction/transactionsData.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import css from './Transactions.module.css';
+import PropTypes from 'prop-types';
+
+const TransactionsHistory = ({ items }) => {
+  return (
+    <div className={css.wrapper}>
+      <table className={css.transactionsHistory}>
+        <thead>
+          <tr>
+            <th>Type</th>
+            <th>Amount</th>
+            <th>Currency</th>
+          </tr>
+        </thead>
+        <tbody>
+          {items.map(transaction => (
+            <TransactionItem key={transaction.id} transaction={transaction} />
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+TransactionsHistory.propTypes = {
+  items: PropTypes.array,
+};
+
+const TransactionItem = ({ transaction }) => {
+  const { type, amount, currency } = transaction;
+
+  return (
+    <tr>
+      <td>{type}</td>
+      <td>{amount}</td>
+      <td>{currency}</td>
+    </tr>
+  );
+};
+TransactionItem.propTypes = {
+  transaction: PropTypes.object,
+};
+
+export default TransactionsHistory;
